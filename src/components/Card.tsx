08be5f599e99b4a82402c7cba67dd29ff2546881
Bradley Kenny <brad.kenny@swipejobs.com>
2021-06-13 11:54:22 +1000

    Forgot to add in previous commit...

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type CardProps = {};
+type CardState = {};
+
+class Card extends React.Component<CardProps, CardState> {
+    
+    render () {
+        return (
+            <div>This will be a card in the future.</div>
+        );
+    }
+
+}
+
+export default Card;
