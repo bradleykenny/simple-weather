557243713f6083a6b7962a19cf930b875d6ac573
Bradley Kenny <brad.kenny@swipejobs.com>
2021-06-23 21:38:08 +1000

    Transitioned from class components to functional components and other updates to data rep.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
+import twemoji from 'twemoji';
+import parse from 'html-react-parser';
+import { DailyWeather } from '../types/DailyWeather';
 
 type CardProps = {
-    day: string,
-    description: string,
-    avgTemp: string,
-    minTemp: string,
-    maxTemp: string,
-    icon: string
+    weather: DailyWeather
 };
-type CardState = {};
 
-class Card extends React.Component<CardProps, CardState> {
+const Card = (props: CardProps) => {
+
+    const { weather } = props;
+
+    // const iconUrl: string = `http://openweathermap.org/img/wn/${ weather.weather[0].icon}@4x.png`;
+    const emojiIcon: string = twemoji.parse("⛅️",  { folder: 'svg', ext: '.svg' });
     
-    render () {
-        const iconUrl: string = `http://openweathermap.org/img/wn/${this.props.icon}@4x.png`;
-        
-        return (
-            <div className="w-80 px-8 pt-8 pb-4 bg-white shadow-md rounded-md">
-                <h1 className="text-2xl mb-2">{this.props.day}</h1>
-                <h2 className="text-8xl mb-2">{this.props.avgTemp}ºC</h2>
-                <h2 className="text-gray-500 italic">{this.props.description}</h2>
-                <img src={iconUrl} className="m-auto" />
+    return (
+        <div className="w-80 px-8 pt-8 pb-4 bg-white shadow-md rounded-md">
+            <h1 className="text-2xl mb-2">Tuesday</h1> {/* TODO: coming soon ... */}
+            <h2 className="text-8xl mb-2">{ weather.temp.day }ºC</h2>
+            <h2 className="text-gray-500 italic">{ weather.weather[0].description }</h2>
+            <div className="mx-auto mt-6 mb-6 w-3/6">
+                { parse(emojiIcon) }
             </div>
-        );
-    }
+        </div>
+    );
 
 }
 
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { WeatherData } from '../types/WeatherData';
 
 type CarouselProps = {};
-type CarouselState = {};
 
-class Carousel extends React.Component<CarouselProps, CarouselState> {
+const Carousel = (props: CarouselProps) => {
+    const [ weather, setWeather ] = useState<WeatherData>();
+
+    const apiUrl: string = `https://api.openweathermap.org/data/2.5/onecall?lat=-33.839490&lon=151.209080&appid=${process.env.REACT_APP_API}`;
+    const weatherData: Promise<WeatherData> = fetch(apiUrl).then(res => res.json()).then(res => res);
+
+    weatherData.then(wd => {
+        setWeather(wd);
+    });
     
-    render () {
-        const apiUrl: string = `https://api.openweathermap.org/data/2.5/onecall?lat=-33.839490&lon=151.209080&appid=${process.env.REACT_APP_API}`;
-        const weatherData = fetch(apiUrl).then(res => res.json()).then(res => res);
+    return (
+        <>
+            { weather?.daily[0].feels_like.day }
+        </>
+    );
 
-        console.log(weatherData);
-        
-        return (
-            <></>
-        );
-    }
 
 }
 
